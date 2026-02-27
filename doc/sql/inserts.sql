6f7ca2ab6b991b951e9fd230d947b51d3133d770
Raul Sanchez <rsanchelop2@educacion.navarra.es>
2026-02-27 09:28:05 +0100

    Nueva tabla en la base de datos e inserts

--- doc/sql/inserts.sql
+++ doc/sql/inserts.sql
@@ -1,2 +1,8 @@
+
+-- INSERTS TABLA USUARIOS
 insert into users (username, password, rol) values ("admin", "admin",1);
 insert into users (username, password, rol) values ("user", "1234",2);
+
+-- INSERTS TABLE
+insert into teams (name, sede) values ("Real Madrid", "Bernabeu");
+insert into teams (name, sede) values ("FC Barcelona", "Camp nou");
