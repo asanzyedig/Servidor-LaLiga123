c9bcdd87a4a2a4894924f5f9da8e8a5642306959
asanzyedig <asanzyedig1@educacion.navarra.es>
2026-02-27 09:23:27 +0100

    Ver Lista Usuarios hecho

--- src/main/java/edu/masanz/laliga123/controller/UsersController.java
+++ src/main/java/edu/masanz/laliga123/controller/UsersController.java
@@ -1,11 +1,14 @@
 package edu.masanz.laliga123.controller;
 
 
+import edu.masanz.laliga123.dao.UsersDao;
+import edu.masanz.laliga123.model.User;
 import edu.masanz.laliga123.service.UsersService;
 import io.javalin.http.Context;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UsersController {
@@ -28,18 +31,13 @@ public class UsersController {
 
     }
 
-    public static void users(@NotNull Context context) {
-
-
-        context.render("/templates/users.ftl");
-    }
-
     public static void listarUsers(@NotNull Context context) {
-
         Map<String, Object> model = new HashMap<>();
 
+        List<User> users = UsersDao.obtenerUsers();
+        model.put("usuarios", users);
 
-        context.render("/templates/lista-users.ftl");
+        context.render("/templates/lista-users.ftl", model);
     }
 
     public static void editaUser(@NotNull Context context) {
